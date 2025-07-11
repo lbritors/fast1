0af1b9ce05755d9bff01f3fbdb79a94c2b8345fb
lbritors <britoleticiars@gmail.com>
2025-07-11 18:06:31 -0300

    feat: add schemas

--- fast1/app.py
+++ fast1/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fast1.schemas import Message
+from http import HTTPStatus
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Servidor rodando'}
 
