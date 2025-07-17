dc3528064675f6beeeec1f4e1a4fec9c777b2c23
lbritors <britoleticiars@gmail.com>
2025-07-17 14:37:27 -0300

    feat: add testes para verificar funcionamento do db

---
+++ tests/test_db.py
@@ -0,0 +1,24 @@
+from sqlalchemy import select
+from dataclasses import asdict
+
+from fast1.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='luna', password='123', email='luna@luna.com')
+        session.add(new_user)
+        session.commit()
+    
+
+    user = session.scalar(select(User).where(User.username == 'luna'))
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'luna',
+        'password': '123',
+        'email': 'luna@luna.com',
+        'created_at': time
+    }
+
+
+
