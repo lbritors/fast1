46d57ab1a140fa7dc25201fbdbb7a77a3a16df02
lbritors <britoleticiars@gmail.com>
2025-07-17 16:22:02 -0300

    feat: add updated_at to users

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,23 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session
-from datetime import datetime
-from contextlib import contextmanager
+
 from fast1.app import app
 from fast1.models import table_registry
 
 
 @contextmanager
-def _mock_db_time(*, model, time=datetime(2025, 07, 15)):
+def _mock_db_time(*, model, time=datetime(2025, 7, 15)):
     def fake_time_hook(mapper, connection, target):
         if hasattr(target, 'created_at'):
             target.created_at = time
-    
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
     event.listen(model, 'before_insert', fake_time_hook)
     yield time
     event.remove(model, 'before_insert', fake_time_hook)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import select
 from dataclasses import asdict
+
+from sqlalchemy import select
 
 from fast1.models import User
 
@@ -9,7 +10,6 @@ def test_create_user(session, mock_db_time):
         new_user = User(username='luna', password='123', email='luna@luna.com')
         session.add(new_user)
         session.commit()
-    
 
     user = session.scalar(select(User).where(User.username == 'luna'))
     assert asdict(user) == {
@@ -17,8 +17,6 @@ def test_create_user(session, mock_db_time):
         'username': 'luna',
         'password': '123',
         'email': 'luna@luna.com',
-        'created_at': time
+        'created_at': time,
+        'updated_at': time,
     }
-
-
-
